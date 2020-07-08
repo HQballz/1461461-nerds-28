b0b6e7c62e9561268d133a19558c50c8ded4558f
Maxim Shvets <hqballz@gmail.com>
2020-07-08 18:16:21 +0300

    Оживляю слайдер

--- js/popup.js
+++ js/popup.js
@@ -1,60 +1,74 @@
-var popupLink = document.querySelector(".button-address");
-var popupWindow = document.querySelector(".popup-window");
-var popupClose = popupWindow.querySelector(".cross");
-var popupForm = popupWindow.querySelector(".popup-form")
-var popupName = popupWindow.querySelector(".name");
-var popupEmail = popupWindow.querySelector(".email")
+var popupLink = document.querySelector('.button-address');
+var popupWindow = document.querySelector('.popup-window');
+var popupClosebutton = popupWindow.querySelector('.cross');
+var popupForm = popupWindow.querySelector('.popup-form')
+var popupName = popupWindow.querySelector('.name');
+var popupEmail = popupWindow.querySelector('.email')
+var popupTextarea = popupWindow.querySelector('.textarea')
+
 
 var isStorageSupport = true;
-var storage = "";
+var storage = '';
+
+
+function popupClose() {
+    popupWindow.classList.remove('popup-window-show');
+    popupWindow.classList.remove('popup-window-error');
+}
 
 try {
-    storage = localStorage.getItem("name");
-    storage = localStorage.getItem("email");
+    storageName = localStorage.getItem('name');
+    storageEmail = localStorage.getItem('email');
 } catch (err) {
     isStorageSupport = false;
 }
 
-popupLink.addEventListener("click", function (evt) {
+popupLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popupWindow.classList.add("popup-window-show");
+    popupWindow.classList.add('popup-window-show');
+    popupName.removeAttribute('required');
+    popupEmail.removeAttribute('required');
 
     if (storage) {
-        popupName.value = storage;
-        popupEmail.value = storage;
+        popupName.value = storageName;
     } else {
         popupEmail.focus();
+    }
+    if (storage) {
+        popupEmail.value = storageEmail;
+    } else {
+        popupTextarea.focus();
     }
 
     popupName.focus();
 });
 
-popupClose.addEventListener("click", function (evt) {
+popupClosebutton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    popupWindow.classList.remove("popup-window-show");
-    popupWindow.classList.remove("popup-window-error");
+    popupClose();
 });
 
-popupForm.addEventListener("submit", function (evt) {
+popupForm.addEventListener('submit', function (evt) {
     if (!popupName.value || !popupEmail.value) {
         evt.preventDefault();
-        popupWindow.classList.remove("popup-window-error");
-        popupWindow.offsetWidth = loginPopup.offsetWidth;
-        popupWindow.classList.add("popup-window-error");
+        popupWindow.classList.remove('popup-window-error');
+        popupWindow.offsetWidth = popupWindow.offsetWidth;
+        popupWindow.classList.add('popup-window-error');
+        popupName.classList.add('popup-error');
+        popupEmail.classList.add('popup-error');
     } else {
+        popupName.classList.remove('popup-error');
+        popupEmail.classList.remove('popup-error');
         if (isStorageSupport) {
-            localStorage.setItem("name", popupName.value);
-            localStorage.setItem("email", popupEmail.value);
+            localStorage.setItem('name', popupName.value);
+            localStorage.setItem('email', popupEmail.value);
         }
     }
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (popupWindow.classList.contains("popup-window-show")) {
-            evt.preventDefault();
-            popupWindow.classList.remove("popup-window-show");
-            popupWindow.classList.remove("popup-window-error");
-        }
+window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27 && popupWindow.classList.contains('popup-window-show')) {
+        evt.preventDefault();
+        popupClose();
     }
 });
